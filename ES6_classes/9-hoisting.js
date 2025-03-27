c972dfe60aa242016d5217d7c80a12a02960a70b
Jonas-Jungling <jonas.jungling@oclock.school>
2025-03-27 22:44:56 +0100

    refactor task9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -13,8 +13,6 @@ export class HolbertonClass {
   }
 }
 
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
   constructor(firstName, lastName, holbertonClass) {
@@ -22,19 +20,21 @@ export class StudentHolberton {
     this._lastName = lastName;
     this._holbertonClass = holbertonClass;
   }
-
+  
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
-
+  
   get holbertonClass() {
     return `${this._holbertonClass}`;
   }
-
+  
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
