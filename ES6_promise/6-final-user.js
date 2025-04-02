b1f311fe9bda4e28dcfb29f003385c3a1a434cd2
Jonas-Jungling <jonas.jungling@oclock.school>
2025-04-02 13:54:14 +0200

    update task6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,18 +1,18 @@
-import signUpUser from "./4-user-promise";
+import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName){
-    const results = await Promise.allSettled([
-        signUpUser(firstName, lastName),
-        uploadPhoto(fileName),
-    ]);
-    const y = [];
-    results.forEach(element => {
-        y.push({
-            status: element.status,
-            value: element.value || String(element.reason),
-        });
-    });
-
-    return y
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const array = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  return array.map((result) => {
+    if (result.status === 'fulfilled') {
+      return {
+        status: result.status,
+        value: result.value,
+      };
+    }
+    return {
+      status: result.status,
+      value: String(result.reason),
+    };
+  });
 }
