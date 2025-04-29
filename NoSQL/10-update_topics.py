435e47a8c82b064d9d762cbd64ef12986c8f7de0
Jonas-Jungling <jonas.jungling@oclock.school>
2025-04-29 14:50:06 +0200

    Add the firsts 11 tasks

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Module to update the topics of a document in
 a MongoDB collection.
 """
@@ -8,4 +8,7 @@ def update_topics(mongo_collection, name, topics):
     """
     Updates the topics of a document in a MongoDB collection.
     """
-    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
