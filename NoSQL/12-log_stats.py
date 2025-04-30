874b82f6069b9404c6009930b21c0e1b044de263
Jonas-Jungling <jonas.jungling@oclock.school>
2025-04-30 10:56:49 +0200

    Add last task in the project

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,2 +1,32 @@
 #!/usr/bin/env python3
+"""This script connects to a MongoDB database, retrieves log statistics
+from an 'nginx' collection, and prints the results."""
+from pymongo import MongoClient
 
+
+def print_logs():
+    """Connects to the MongoDB 'nginx' collection, retrieves log statistics,
+    and prints the total number of logs, counts of HTTP methods, and
+    status checks."""
+    client = MongoClient()
+    collection = client.logs.nginx
+
+    logs = collection.count_documents({})
+    get = collection.count_documents({"method": "GET"})
+    post = collection.count_documents({"method": "POST"})
+    put = collection.count_documents({"method": "PUT"})
+    patch = collection.count_documents({"method": "PATCH"})
+    delete = collection.count_documents({"method": "DELETE"})
+    status = collection.count_documents({"method": "GET", "path": "/status"})
+
+    print("{} logs".format(logs))
+    print("Methods:")
+    print("\t method GET: {}".format(get))
+    print("\t method POST: {}".format(post))
+    print("\t method PUT: {}".format(put))
+    print("\t method PATCH: {}".format(patch))
+    print("\t method DELETE: {}".format(delete))
+    print("{} status check".format(status))
+
+
+print_logs()
