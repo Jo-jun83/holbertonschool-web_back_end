435198b445489f8d456e939fe1587ba7c5ff8c9b
Jonas-Jungling <jonas.jungling@oclock.school>
2025-05-19 14:22:34 +0200

    Add task 0 to 2

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+function countStudents(path) {
+    try {
+        const data = fs.readFileSync(path, 'utf8').trim();
+
+        const lines = data.split('\n').filter(line => line.trim() !== '');
+
+        const students = lines.slice(1);
+
+        console.log(`Number of students: ${students.length}`);
+
+        const fields = {};
+        for (const line of students) {
+            const [firstName, lastName, age, field] = line.split(',');
+            if (!fields[field]) {
+                fields[field] = [];
+            }
+            fields[field].push(firstName);
+        }
+
+        for (const field in fields) {
+            const names = fields[field].join(', ');
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${names}`);
+        }
+    } catch (error) {
+        throw new Error('Cannot load the database');
+    }
+}
+
+module.exports = countStudents;
