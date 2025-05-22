afc3f36487fc03df1df1eb7b1de25755ffee5e69
Jonas-Jungling <jonas.jungling@oclock.school>
2025-05-22 10:59:08 +0200

    refactor task 5

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,44 +1,24 @@
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const countStudents = require('./3-read_file_async');
 
-const DATABASE = path.join(__dirname, 'database.csv');
-
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    try {
-      const data = fs.readFileSync(DATABASE, 'utf8').trim();
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
-
-      const fields = {};
-      for (const line of students) {
-        const [firstName, , , field] = line.split(',');
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(firstName);
-      }
-
-      let response = 'This is the list of our students\n';
-      response += `Number of students: ${students.length}\n`;
-      for (const field in fields) {
-        if (Object.prototype.hasOwnProperty.call(fields, field)) {
-          response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-        }
-      }
-
-      res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.end(response.trim());
-    } catch (error) {
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('Cannot load the database');
+    if (process.argv[2]) {
+      countStudents(process.argv[2])
+        .then((data) => {
+          res.end(`This is the list of our students\n${data}`);
+        })
+        .catch((error) => {
+          res.end(`This is the list of our students\n${error.message}`);
+        });
+    } else {
+      res.end('This is the list of our students\nCannot load the database');
     }
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found');
   }
 });
-
 app.listen(1245);
+
+module.exports = app;
