c5705a5534e1a8932e7f5c359850d2c8111e68bf
Jonas-Jungling <jonas.jungling@oclock.school>
2025-05-22 10:57:18 +0200

    refactor task 7

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,40 +1,21 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const countStudents = require('./3-read_file_async');
 
 const app = express();
-const PORT = 1245;
-const DATABASE = path.join(__dirname, 'database.csv');
-
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello Holberton School!');
 });
-
 app.get('/students', (req, res) => {
-  try {
-    const data = fs.readFileSync(DATABASE, 'utf8').trim();
-    const lines = data.split('\n').filter((line) => line.trim() !== '');
-    const students = lines.slice(1);
-
-    const fields = {};
-    for (const line of students) {
-      const [firstName, , , field] = line.split(',');
-      if (!fields[field]) fields[field] = [];
-      fields[field].push(firstName);
-    }
-
-    let response = 'This is the list of our students\n';
-    response += `Number of students: ${students.length}\n`;
-
-    for (const field in fields) {
-      response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-    }
-
-    res.status(200).send(response.trim());
-  } catch (error) {
-    res.status(500).send('Cannot load the database');
-  }
+  countStudents(process.argv[2])
+    .then((data) => {
+      res.set('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${data}`);
+    })
+    .catch((error) => {
+      res.set('Content-Type', 'text/plain');
+      res.send(`This is the list of our students\n${error.message}`);
+    });
 });
-
-app.listen(PORT, () => {
-});
+app.listen(1245);
+module.exports = app;
